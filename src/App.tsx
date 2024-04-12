ee2c564f3e45bc7e1192bc61b5d7fe0deb07074b
DingX2 <pourding@naver.com>
2024-04-13 02:39:20 +0900

    chore(lint-staged): lint-staged 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { useState } from 'react';
+import { type FC, useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import viteLogo from './vite.svg';
 import './App.css';
 
-function App() {
+export const App: FC = () => {
     const [count, setCount] = useState(0);
 
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button type="button" onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
@@ -26,6 +28,4 @@ function App() {
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
         </>
     );
-}
-
-export default App;
+};
