9a78f15fec9f4ef6c6b8dcc133d8732ee1b3beee
DingX2 <pourding@naver.com>
2024-04-15 01:28:42 +0900

    docs: jsdoc 추가

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,3 +1,10 @@
+/**
+ * 숫자를 계산해주는 훅
+ * @param {number} value
+ * @returns {number} 계산된 값
+ * @example
+ * const { increase, decrease } = useCounter(10);
+ */
 export const useCounter = (value: number) => {
     return { increase: value + 1, decrease: value - 1 };
 };
--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,3 +1,9 @@
+/**
+ * 2로 나눈 나머지를 반환하는 훅
+ * @param {number} value
+ * @returns {number} 2로 나눈 나머지
+ */
+
 export const mod = (value: number) => {
     return value % 2;
 };
